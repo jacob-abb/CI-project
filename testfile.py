d436e8474eda74981d54c233ad779c9765d36b3e
jacob-abb <Jacob-xiao.yuan@cn.abb.com>
2024-07-25 16:34:06 +0800

    Update testfile.py

--- testfile.py
+++ testfile.py
@@ -21,86 +21,90 @@ if len(sys.argv) < 2:
 print("Script name:", sys.argv[0])  
 for i in range(1, len(sys.argv)):  
     print("Parameter", i, ":", sys.argv[i])
-  
-# def query_workitems(ql_str):
 
-#    # Azure DevOps组织URL（注意替换为你的组织名）  
-#    organization_url = "https://dev.azure.com/ABB-BCI-PCP/"  
-#    # Azure DevOps项目的名称  
-#    project_name = "PCP-Test"  
-#    # 你的个人访问令牌（PAT）  
-#    personal_access_token = ""  
-#    wiql_url = f'{organization_url}/{project_name}/_apis/wit/wiql?api-version=6.0'  
+organization = sys.argv[0]
+project = sys.argv[1]
+query_id = sys.argv[2]
 
-#    wiql = json.loads(ql_str)
-#    print(wiql_url, wiql)
-#    # 发送POST请求以执行Wiql查询  
-#    headers = {  
-#       'Content-Type': 'application/json',  
-#    }  
-#    response = requests.post(wiql_url, headers=headers, json=wiql, auth=HTTPBasicAuth('', personal_access_token))  
+def query_workitems(ql_str):
 
-#    # 检查响应状态码  
-#    if response.status_code == 200:  
-#       # 解析响应内容  
-#       work_items = response.json()['workItems']  
-#       for work_item in work_items[0:1]:  
-#          print(work_item)
-#          #print(f"Work Item ID: {work_item['id']}, Title: {work_item['fields']['System.Title']}")  
+   # Azure DevOps组织URL（注意替换为你的组织名）  
+   organization_url = organization
+   # Azure DevOps项目的名称  
+   project_name = project 
+   # 你的个人访问令牌（PAT）  
+   personal_access_token = query_id 
+   wiql_url = f'{organization_url}/{project_name}/_apis/wit/wiql?api-version=6.0'  
 
-#          # 构造获取工作项详情的URL  
-#          #print(work_item['url'].split("/")[-1])				  
-#          wiql_url = f'{organization_url}/{project_name}/_apis/wit/workItems/{work_item['url'].split("/")[-1]}'  
-#          # 创建头部信息  
-#          encoded_credentials = base64.b64encode(f":{personal_access_token}".encode('ascii')).decode('ascii')  
+   wiql = json.loads(ql_str)
+   print(wiql_url, wiql)
+   # 发送POST请求以执行Wiql查询  
+   headers = {  
+      'Content-Type': 'application/json',  
+   }  
+   response = requests.post(wiql_url, headers=headers, json=wiql, auth=HTTPBasicAuth('', personal_access_token))  
 
-#          headers = {  
-#             'Authorization': f'Basic {encoded_credentials}',  
-#             'Content-Type': 'application/json'  
-#          }  
-#          #print(wiql_url)
-#          response = requests.get(wiql_url, headers=headers) 
-#          # 检查响应状态码  
-#          if response.status_code == 200:  
-#             # 解析响应内容  
-#             work_item = response.json()  
-#             # 打印工作项的详细信息  
-#             info = json.dumps(work_item, indent=4) 
-#             #print(info)
-#             print(work_item['_links']['workItemType']['href'])
+   # 检查响应状态码  
+   if response.status_code == 200:  
+      # 解析响应内容  
+      work_items = response.json()['workItems']  
+      for work_item in work_items[0:1]:  
+         print(work_item)
+         #print(f"Work Item ID: {work_item['id']}, Title: {work_item['fields']['System.Title']}")  
 
-#             response = requests.get(work_item['_links']['workItemType']['href'], headers=headers) 
-#             if response.status_code == 200:  
-#                # 解析响应内容  
-#                work_item = response.json()  
-#                info = json.dumps(work_item, indent=4) 
+         # 构造获取工作项详情的URL  
+         #print(work_item['url'].split("/")[-1])				  
+         wiql_url = f'{organization_url}/{project_name}/_apis/wit/workItems/{work_item['url'].split("/")[-1]}'  
+         # 创建头部信息  
+         encoded_credentials = base64.b64encode(f":{personal_access_token}".encode('ascii')).decode('ascii')  
 
-#                print(work_item["transitions"])
-#                for con in work_item["states"]:
-#                   print(con)
-#                print(work_item["states"])
-#                for con in work_item["states"]:
-#                   print(con)
-#          else:  
-#             print(f"Failed to retrieve work item details. Status code: {response.status_code}")  
-#             print("Response content:")  
-#             print(response.text)
+         headers = {  
+            'Authorization': f'Basic {encoded_credentials}',  
+            'Content-Type': 'application/json'  
+         }  
+         #print(wiql_url)
+         response = requests.get(wiql_url, headers=headers) 
+         # 检查响应状态码  
+         if response.status_code == 200:  
+            # 解析响应内容  
+            work_item = response.json()  
+            # 打印工作项的详细信息  
+            info = json.dumps(work_item, indent=4) 
+            #print(info)
+            print(work_item['_links']['workItemType']['href'])
 
-#    else:  
-#       print(f"Failed to retrieve work items. Status code: {response.status_code}, Error: {response.text}")
+            response = requests.get(work_item['_links']['workItemType']['href'], headers=headers) 
+            if response.status_code == 200:  
+               # 解析响应内容  
+               work_item = response.json()  
+               info = json.dumps(work_item, indent=4) 
 
-# ql_str = """  
-# {  
-#    "query": "SELECT [System.Id], [System.Title], [System.State] FROM WorkItems WHERE [System.WorkItemType] IN ('Epic', 'Feature') ORDER BY [System.Id]"  
-# }  
-# """  
+               print(work_item["transitions"])
+               for con in work_item["states"]:
+                  print(con)
+               print(work_item["states"])
+               for con in work_item["states"]:
+                  print(con)
+         else:  
+            print(f"Failed to retrieve work item details. Status code: {response.status_code}")  
+            print("Response content:")  
+            print(response.text)
 
-# #query_workitems(ql_str)
+   else:  
+      print(f"Failed to retrieve work items. Status code: {response.status_code}, Error: {response.text}")
 
-# # 构造WIQL查询以获取Epic和Feature的基本信息  
-# ql_str = """
-# {  
-#    "query": "Select [System.Id], [System.Title] From WorkItems Where [System.WorkItemType] = 'User Story' AND [System.State] <> 'Closed' ORDER BY [System.ChangedDate] DESC"  
-# } 
-# """ 
-# #query_workitems(ql_str)
+ql_str = """  
+{  
+   "query": "SELECT [System.Id], [System.Title], [System.State] FROM WorkItems WHERE [System.WorkItemType] IN ('Epic', 'Feature') ORDER BY [System.Id]"  
+}  
+"""  
+
+#query_workitems(ql_str)
+
+# 构造WIQL查询以获取Epic和Feature的基本信息  
+ql_str = """
+{  
+   "query": "Select [System.Id], [System.Title] From WorkItems Where [System.WorkItemType] = 'User Story' AND [System.State] <> 'Closed' ORDER BY [System.ChangedDate] DESC"  
+} 
+""" 
+#query_workitems(ql_str)
